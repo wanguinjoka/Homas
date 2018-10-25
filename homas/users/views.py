625bd263ddb5bee5674f4c9ddcf8ba56e00bf519
wangui.njoka <wangui.njoka@gmail.com>
2018-10-25 11:24:22 +0300

    authentification working

--- homas/users/views.py
+++ homas/users/views.py
@@ -10,7 +10,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}! You are now able to login')
-            return redirect('profile-create')
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
